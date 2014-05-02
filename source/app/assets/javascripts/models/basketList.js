fb8b1437f85c0ba0d2b388b61e349f0c3bfb6e69
Greg Gerla <apprentice@dbc28.local>
2014-05-02 12:14:10 -0700

    printing out basket working

--- source/app/assets/javascripts/models/basketList.js
+++ source/app/assets/javascripts/models/basketList.js
@@ -1,11 +1,28 @@
-function BasketList(){
+function Basket(){
 	this.basket = []
 }
 
-BasketList.prototype = {
+Basket.prototype = {
 	addFoodtoBasket: function(object){
     this.basket.push(object)
+  },
+  saveBasketToDataBase: function(){
+    $.ajax({
+      url: '/home/create',
+      data: this.basket,
+      type: 'POST'})
+      .done(function(json){
+        console.log('database save successful')
+      })
+    }
   }
 }
+
+
+// 1)FoodController has all search Results
+// 2)Clicked food becomes SelectedFood
+//   i) Selected food saves to server in
+//   ii) Basket contents comes back from server
+//   iii) Print out basket server contents
 
 
