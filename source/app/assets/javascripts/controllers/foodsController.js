265f5634ce337705a52fff059d656d08a653a5c7
Greg Gerla <apprentice@dbc28.local>
2014-05-02 20:14:13 -0700

    New template for baskets, basket persists throughout experience

--- source/app/assets/javascripts/controllers/foodsController.js
+++ source/app/assets/javascripts/controllers/foodsController.js
@@ -15,13 +15,12 @@ FoodController.prototype = {
     var basket = this.basket
     var basketView = this.basketView
     // on foodsList event created within foodList model, create template with model's elements AKA foodView (because you are binding the function to foodView)
-    // $(document).on("load", basket.retrieveFoodsFromDataBase())
+    $(document).ready(basket.retrieveFoodsFromDataBase.bind(basket))
+    $(document).on("oldList", this.printSavedBasket.bind(this))
     $('#search-form').on('submit', this.searchFoods.bind(food))
     $(document).on("foodList", foodView.drawFoods.bind(foodView));
     $(document).on("foodList", this.createFoodList.bind(this))
     $(document).on('click', '.food_div', this.findFoodInSearchResults.bind(this))
-    $(document).on('click', '.food_div', this.printOutBasket.bind(this))
-    // $(document).on('click', '.food_div', this.saveBasketToDataBase.bind(this))
   },
   searchFoods: function (event){
     event.preventDefault();
@@ -39,12 +38,11 @@ FoodController.prototype = {
     for(x = 0; x < this.allFoodResults.hits.length; x++){
       if (this.allFoodResults.hits[x]._id === food_id){
         this.basket.addFoodtoBasket(this.allFoodResults.hits[x])
-        console.log(this.basket, "food added to basket")
       }
     }
   },
-  printOutBasket: function(e){
-    this.basketView.drawBasket(this.basket)
+  printSavedBasket: function(e){
+    this.basketView.drawOldBasket(this.basket)
   },
   saveBasketToDataBase: function(e){
     this.basket.savetheBasketToDataBase()
