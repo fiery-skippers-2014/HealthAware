c9dab8f37de6c8e2b5725087e2c67dc6962e2b40
greg <greg@gregs-macbook.local>
2014-05-03 23:16:40 -0700

    adding limit to what is brought across

--- source/app/controllers/goals_controller.rb
+++ source/app/controllers/goals_controller.rb
@@ -9,8 +9,10 @@ class GoalsController < ApplicationController
     @goal.nutrient_id = params[:goal][:nutrient_id]
     @goal.user_id = current_user.id
     if params[:amount] == "FDA"
-      @goal.target = 200
-      @goal.limit = false
+      
+      @goal.target = @goal.nutrient.FDA_recommendation
+      @goal.limit = @goal.nutrient.FDA_limit
+      @goal.unit = @goal.nutrient.unit
     else
       @goal.target = params[:goal][:target]
         if params[:limit][:limit_id] == "minimum"
