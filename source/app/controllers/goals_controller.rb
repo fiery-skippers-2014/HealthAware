585b7923c7b91fbf56cbfdd8f020d90a275f17d6
Greg Gerla <apprentice@dbc28.local>
2014-05-03 21:36:25 -0700

    changed new goal addition

--- source/app/controllers/goals_controller.rb
+++ source/app/controllers/goals_controller.rb
@@ -7,16 +7,18 @@ class GoalsController < ApplicationController
 
   def create
     @goal = Goal.new
-    p params[:goal][:nutrient_id]
-
-        p "params" * 50
     @goal.nutrient_id = params[:goal][:nutrient_id]
     @goal.user_id = current_user.id
-    @goal.limit = params[:limit][:limit_id]
     if params[:amount] == "FDA"
-      @goal.target = 1000
+      @goal.target = 200
+      @goal.limit = false
     else
       @goal.target = params[:goal][:target]
+        if params[:limit][:limit_id] == "minimum"
+          @goal.limit = true
+        else
+          @goal.limit = false
+        end
     end
     @goal.save!
   end
